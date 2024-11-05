ad6f20319bf39581a20b24894256bf3ba5fafcc2
sericakitty <63978546+sericakitty@users.noreply.github.com>
2024-11-05 23:01:02 +0200

    Update to have navigation

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import BottomTabNavigator from './src/navigation/BottomTabNavigator';
-import { StatusBar } from 'expo-status-bar';
+
+import LoginView from './src/views/LoginView';
 
 const App = () => {
+  // State to keep track of user login status, later to be replaced with user provider
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <NavigationContainer>
-      <BottomTabNavigator />
-      <StatusBar style="auto" />
+      {isLoggedIn ? (
+        <BottomTabNavigator />
+      ) : (
+        <LoginView onLoginSuccess={() => setIsLoggedIn(true)} />
+      )}
     </NavigationContainer>
   );
 };
